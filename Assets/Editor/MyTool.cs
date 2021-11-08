f57396422e8b8aa02febf7b8bba8504c5b93706e
kksumm123 <kksumm123@gmail.com>
2021-11-08 16:52:57 +0900

    GUILayout 초안 완료

--- Assets/Editor/MyTool.cs
+++ Assets/Editor/MyTool.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
@@ -6,4 +7,24 @@ using UnityEngine;
 public class MyTool : EditorWindow
 {
 
+    [MenuItem("Tools/MyTool")]
+    static void MyMenu()
+    {
+        Debug.Log("나 불려써?");
+        GetWindow(typeof(MyTool));
+    }
+    string goldString;
+    void OnGUI()
+    {
+        GUILayout.BeginHorizontal();
+        {
+            GUILayout.Label("골드\b\b");
+            goldString = GUILayout.TextField(goldString, 10);
+            if (GUILayout.Button("지급하기"))
+            {
+                Debug.Log($"{goldString} 골드 주까?");
+            }
+        }
+        GUILayout.EndHorizontal();
+    }
 }
