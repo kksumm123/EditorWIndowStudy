6f61a15ea5f6c72f2097837a352859aae1a4d1b0
kksumm123 <kksumm123@gmail.com>
2021-11-08 16:28:33 +0900

    EditorWindow 상속

--- Assets/Editor/MyTool.cs
+++ Assets/Editor/MyTool.cs
@@ -1,18 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
-public class MyTool : MonoBehaviour
+public class MyTool : EditorWindow
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
